c8bfb26c65d57f53d293c38c760c27c1fd6b4626
Phoenix6296 <krishnabiswakarmasc@gmail.com>
2022-05-16 16:20:57 +0530

    Fixed the background of the register modal

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,31 +1,40 @@
 import React from "react";
 import "./Register.css";
+import Navbar from "../Home/Navbar/Navbar";
+import BannerImage from '../../Assets/bannerImage.svg'
 
 const Register = () => {
   return (
-    <div className="register">
-      <div className="r-container">
-        <span>Registration Form</span>
-        <form action="">
-          <div className="r-inputs">
-            <input type="text" placeholder="First Name" required />
-            <input type="text" placeholder="Last Name" required />
-          </div>
-          <div className="r-inputs">
-            <input type="number" placeholder="Age" />
-            <input type="date" placeholder="Date of Birth" required />
-          </div>
-          <div className="r-inputs">
-            <input type="email" placeholder="Email" id="r-email" required />
-            <input type="password" placeholder="Password" required />
-          </div>
-          <div className="r-button">
-            <button>Upload Resume!</button>
-            <button id="r-submit">Submit</button>
-          </div>
-        </form>
+    <>
+      <Navbar />
+      <div className="register">
+        <span>
+          Placement <span>+</span>
+        </span>
+        <div className="r-container">
+          <span>Registration Form</span>
+          <form action="">
+            <div className="r-inputs">
+              <input type="text" placeholder="First Name" required />
+              <input type="text" placeholder="Last Name" required />
+            </div>
+            <div className="r-inputs">
+              <input type="number" placeholder="Age" />
+              <input type="date" placeholder="Date of Birth" required />
+            </div>
+            <div className="r-inputs">
+              <input type="email" placeholder="Email" id="r-email" required />
+              <input type="password" placeholder="Password" required />
+            </div>
+            <div className="r-button">
+              <button>Upload Resume!</button>
+              <button id="r-submit">Submit</button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+      <img src={BannerImage} alt="" />
+    </>
   );
 };
 
